b78c8fbf9dbc0e3fab01e0606a1bd38ea056244f
gajendra-ingle <gajendraingle01@gmail.com>
2024-02-27 20:18:10 +0530

    First day

---
+++ 30 Days JavaScript Challenge/1-Day.js
@@ -0,0 +1,12 @@
+// Day - 01
+
+// Define a function that takes two parameters
+function sumNumbers(num1, num2) {
+    // Add the two parameters and store the result in a variable
+    const sum = num1 + num2;
+    // Return the sum
+    return sum;
+  }
+  
+  // Call the function and output the result to the console
+  console.log(sumNumbers(5, 10)); // Output: 15
