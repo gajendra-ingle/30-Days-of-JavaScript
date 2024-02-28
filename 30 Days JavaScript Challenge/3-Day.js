9d847b38e8b4512173c7f54705d93e447debbcfb
gajendra-ingle <gajendraingle01@gmail.com>
2024-02-29 01:18:39 +0530

    3rd day solution is add

---
+++ 30 Days JavaScript Challenge/3-Day.js
@@ -0,0 +1,15 @@
+// Declare and initialize two variables with number values
+const num1 = Number(21);
+const num2 = Number(34);
+
+// Declare and initialize four more variables with arithmetic operations performed on num1 and num2
+const sum = num1 + num2; // Add num1 and num2
+const diff = num1 - num2; // Subtract num2 from num1
+const product = num1 * num2; // Multiply num1 and num2
+const quotient = num1 / num2; // Divide num1 by num2
+
+// Output the values of the four variables to the console, with descriptive labels
+console.log(`Sum: ${sum}`); // Sum: 55
+console.log(`Difference: ${diff}`); // Difference: -13
+console.log(`Product: ${product}`); // Product: 714
+console.log(`Quotient: ${quotient}`); // Quotient: 0.6176470588235294
