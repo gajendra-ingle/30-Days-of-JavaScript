a5de475e8103be25ca0ffdf6278a28313f8b16cf
gajendra-ingle <gajendraingle01@gmail.com>
2024-03-08 08:55:31 +0530

    11th Day Solution

---
+++ 30 Days JavaScript Challenge/11-Day.js
@@ -0,0 +1,8 @@
+// Define a function called greet that takes a name parameter
+function greet(name) {
+    // Use a console.log statement to output a greeting message with the name parameter
+    console.log(`Hello ${name}! How are you doing today?`);
+  }
+  
+  // Call the greet function and pass in the name "Pranay" as an argument
+  greet("Gajendra"); // Output: Hello Pranay! How are you doing today?
