f02d0e064ead3cfec31906244324d3528ab4a236
gajendra-ingle <gajendraingle01@gmail.com>
2024-03-19 16:11:17 +0530

    22 day solution

---
+++ 30 Days JavaScript Challenge/22-Day.js
@@ -0,0 +1,15 @@
+/**
+ * Returns a promise which rejects with a specified error message after a specified number of milliseconds.
+ */
+function rejectWithDelay(errorMessage, delay) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error(errorMessage));
+      }, delay);
+    });
+  }
+  
+  // Example usage:
+  rejectWithDelay("Oops! Something went wrong.", 2000).catch((error) => {
+    console.error(error.message); // Output: Oops! Something went wrong.
+  });
