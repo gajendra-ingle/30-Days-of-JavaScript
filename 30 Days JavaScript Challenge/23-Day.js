3183d22935f1a19a814369016e6030a6a6f86c0d
gajendra-ingle <gajendraingle01@gmail.com>
2024-03-20 08:34:21 +0530

    23 Day Solution

---
+++ 30 Days JavaScript Challenge/23-Day.js
@@ -0,0 +1,29 @@
+function getRandomNumbersArray(length, delay) {
+    return new Promise((resolve, reject) => {
+      // Using setTimeout to simulate delay
+      setTimeout(() => {
+        // Creating an array of random numbers of specified length
+        const numbersArray = Array.from({ length }, () => Math.floor(Math.random() * 100));
+        // Resolving the promise with the generated array
+        resolve(numbersArray);
+      }, delay);
+    });
+  }
+  
+  // Testing the function with delay of 2 seconds and array length of 5
+  getRandomNumbersArray(5, 2000)
+    .then((result) => {
+      console.log(result); // Output: [23, 74, 48, 52, 63]
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  
+  // Testing the function with delay of 3 seconds and array length of 10
+  getRandomNumbersArray(10, 3000)
+    .then((result) => {
+      console.log(result); // Output: [79, 5, 64, 46, 2, 55, 33, 71, 28, 38]
+    })
+    .catch((error) => {
+      console.error(error);
+    });
