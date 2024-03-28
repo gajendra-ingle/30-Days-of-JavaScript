d3b5d25d6011b38dd16fba759879dbb6ec8f1786
gajendra-ingle <gajendraingle01@gmail.com>
2024-03-28 09:13:47 +0530

    31 day solution

---
+++ 30 Days JavaScript Challenge/31-Day.js
@@ -0,0 +1,8 @@
+const findMax = (numbers) => {
+    // spread operator used to pass the array elements as arguments to Math.max()
+    return Math.max(...numbers);
+  }
+  
+  // testing the function
+  console.log(findMax([5, 3, 9, 1, 7])); // expected output: 9
+  console.log(findMax([0, -2, 10, 4])); // expected output: 10
