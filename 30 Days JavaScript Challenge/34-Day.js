494d52248004bb5a39a2f1d97a2d66bdfbdd689d
gajendra-ingle <gajendraingle01@gmail.com>
2024-03-31 11:25:35 +0530

    34 day solution

---
+++ 30 Days JavaScript Challenge/34-Day.js
@@ -0,0 +1,17 @@
+function simulateDelay(delay, callback) {
+    setTimeout(callback, delay);
+  }
+  
+  // Example usage:
+  console.log("Before delay");
+  
+  simulateDelay(2000, () => {
+    console.log("After delay");
+  });
+  
+  console.log("Function finished executing");
+  
+  // Output:
+  // Before delay
+  // Function finished executing
+  // After delay (after a 2 second delay)
